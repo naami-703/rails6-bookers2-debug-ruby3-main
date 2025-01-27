8c9ce86f33fc46eecb1df7c247994b3e19823260
naami-703 <192409540+naami-703@users.noreply.github.com>
2025-01-27 00:10:11 +0000

    [Finish]

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  before_action :ensure_correct_user, only: [:update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] =  "You have updated user successfully."
+      redirect_to users_path
+    else
+      render template: "users/edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+
+end
